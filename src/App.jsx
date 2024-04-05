c687a05db4b8c4ef8a7000c64acb7b0b1beb63f4
programmingweb <gomito724@gmail.com>
2024-04-05 09:09:26 -0300

    se modifico el elemento con valor maru

--- src/App.jsx
+++ src/App.jsx
@@ -308,7 +308,7 @@ const unir=letter.join(" ");
      <p>{letter.join("").length}</p>
      <p>{letter.length}</p>
      <button type="button" onClick={handleClose}>cerrar</button>
-     <p>maru</p>   
+     <p>mi familia</p>   
      
      </div>
      </>
